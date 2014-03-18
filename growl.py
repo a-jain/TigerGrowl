cb6b94b45d1524bf4b030f1c67ae3067563ae8be
kbruccoleri <kevinbruccoleri@gmail.com>
2014-03-17 22:01:05 -0400

    3rd Commit
    Added Bootstrap, jQuery Libraries.
    
    Updated home page (index.html)

--- growl.py
+++ growl.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-	return "Index"
+	return render_template('index.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -15,7 +15,6 @@ def hello(name=None):
 
 
 if __name__ == '__main__':
-	app.debug = True
 	app.run()
 
 
