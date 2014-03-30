61381cc1280e37d64b313c0d47607ba40e5a7c2c
kbruccoleri <kevinbruccoleri@gmail.com>
2014-03-30 17:51:51 -0400

    test 6

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def index():
 	import _ssl;_ssl.PROTOCOL_SSLv23 = _ssl.PROTOCOL_SSLv3
 	import CASClient
 
-	return redirect('https://fed.princeton.edu/cas/')
+	return redirect('https://www.facebook.com/')
 	#C = CASClient.CASClient()
 	#netid = C.Authenticate()
 
