cfa2df2c74357e135790e39d4f9632192e1ff235
kbruccoleri <kevinbruccoleri@gmail.com>
2014-03-30 15:40:01 -0400

    get online commit

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+@app.route('/home')
+def index():
+	return render_template('CAS/index.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+	app.run()
