692bd5da67f6337375d14263cfcf64418913a67a
Akash Jain <mr.akashj@gmail.com>
2014-03-30 22:37:06 -0400

    fb

--- application.py
+++ application.py
@@ -9,16 +9,7 @@ application.debug = True
 @application.route('/')
 @application.route('/home')
 def index():
-<<<<<<< HEAD
 	return render_template('landing.html')
-=======
-	import _ssl;_ssl.PROTOCOL_SSLv23 = _ssl.PROTOCOL_SSLv3
-	import CASClient
-
-	return redirect('https://fed.princeton.edu/cas/', code=302)
-	#C = CASClient.CASClient()
-	#netid = C.Authenticate()
->>>>>>> b57bb2702bc9d35fb7cd4bfa1cce76e61d1aaf88
 
 @application.route('/hello/')
 @application.route('/hello/<name>')
