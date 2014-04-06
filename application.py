cee4ebaae74a44d54d75db1c1fd77ed561db0628
Akash Jain <mr.akashj@gmail.com>
2014-04-06 14:34:37 -0400

    General cleanup

--- application.py
+++ application.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flaskext.mysql import MySQL
 
 application = Flask(__name__)
 application.debug = True
+application.config['MYSQL_DATABASE_USER'] = 'root'
+application.config['MYSQL_DATABASE_PASSWORD'] = 'trfoSVTV'
+application.config['MYSQL_DATABASE_DB']
 
 @application.route('/')
 @application.route('/home')
