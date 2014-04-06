d6d219035ae325ea2fe0603f362c16ab0aeac4c5
Akash Jain <mr.akashj@gmail.com>
2014-04-06 16:19:09 -0400

    testing

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ def feedPrototype():
 @application.route('/hello/')
 @application.route('/hello/<name>')
 def hello(name=None):
-	return render_template('hello.html', name=data["firstname"])
+	return render_template('hello.html', name=data[2])
 
 
 if __name__ == '__main__':
