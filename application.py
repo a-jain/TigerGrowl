d3c90831c7de095245c187b9c321fbe3ab99389b
Akash Jain <mr.akashj@gmail.com>
2014-04-06 15:12:03 -0400

    SQL bitch

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
-from flask.ext.mysql import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # mysql = MySQL()
 application = Flask(__name__)
