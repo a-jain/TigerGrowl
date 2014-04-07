7427be695b8d0694c2efe3535995961d483ec133
kbruccoleri <kevinbruccoleri@gmail.com>
2014-04-06 22:20:26 -0400

    MySQLdb

--- application.py
+++ application.py
@@ -3,26 +3,25 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flaskext.mysql import MySQL
+import MySQLdb
 
 mysql = MySQL()
 application = Flask(__name__)
 application.debug = True
 
-application.config['MYSQL_DATABASE_USER'] = 'growladmin'
-application.config['MYSQL_DATABASE_PASSWORD'] = 'youeatyet?' # DO NOT LEAVE PASSWORDS IN CODE
-application.config['MYSQL_DATABASE_DB'] = 'ebdb'
-application.config['MYSQL_DATABASE_HOST'] = 'aa104vf4z8592ny.ct5w0yg0rrlk.us-east-1.rds.amazonaws.com'
-mysql.init_app(application)
 
-db = mysql.connect()
+db = MySQLdb.connect(host="aa104vf4z8592ny.ct5w0yg0rrlk.us-east-1.rds.amazonaws.com",user="growladmin",passwd="youeatyet?",db="ebdb")
+#application.config['MYSQL_DATABASE_USER'] = 'growladmin'
+#application.config['MYSQL_DATABASE_PASSWORD'] = 'youeatyet?' # DO NOT LEAVE PASSWORDS IN CODE
+#application.config['MYSQL_DATABASE_DB'] = 'ebdb'
+#application.config['MYSQL_DATABASE_HOST'] = 'aa104vf4z8592ny.ct5w0yg0rrlk.us-east-1.rds.amazonaws.com'
+#mysql.init_app(application)
+#db = mysql.connect()
 db.autocommit(True)
+
 cursor = db.cursor()
-cursor.execute("SELECT * from ebdb.user_table where firstname='Kevin'")
-data = cursor.fetchone()
-if data is None:
-	print "Nothing was found in the database"
-else:
-	print data
+#cursor.execute("SELECT * from ebdb.user_table where firstname='Kevin'")
+#result = db.store_result()
 
 @application.route('/')
 @application.route('/home')
@@ -40,7 +39,7 @@ def feedPrototype():
 @application.route('/insertDB')
 @application.route('/insertDB/<id>/<firstname>/<surname>/<netid>')
 def dbinsert(id=None, firstname=None, surname=None, netid=None):
-	sql = "INSERT INTO ebdb.user_table (user_id, firstname, lastname, netid) VALUES (30876, 'Akaddsh', 'Jaiddn', 'akasddhj');"
+	sql = "INSERT INTO ebdb.user_table (user_id, firstname, lastname, netid) VALUES (6969, 'Roberto', 'Flamenco', 'qqkk');"
 	cursor.execute(sql)
 	# db.commit()
 	cursor.execute("SELECT * FROM ebdb.user_table;")
