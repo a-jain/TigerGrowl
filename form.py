a112f901371855a7d4e8b642480cfc63569bb952
Akash Jain <mr.akashj@gmail.com>
2014-04-10 01:56:01 -0400

    reqs

--- form.py
+++ form.py
@@ -1,12 +1,11 @@
 from wtforms import Form, BooleanField, TextField, IntegerField, validators
 from wtforms.validators import ValidationError, Required, Email, URL
 from wtforms.fields import HiddenField
-from wtforms_components import TimeField
 
 class MealForm(Form):
     mealtable = IntegerField('Meal ID', [Required(), validators.NumberRange(min=40, max=110000000)])
-    host = TextField('Host', [Required(), validators.Length(min=3, max=35)])
-    place = TextField('Place', [Required(), validators.Length(min=3, max=35)])
+    host = TextField('Host', [Required(), validators.Length(min=3, max=45)])
+    place = TextField('Place', [Required(), validators.Length(min=3, max=45)])
     time = TimeField('Time')
     date = TextField('Date')
 
@@ -15,8 +14,8 @@ def validate_email(form, field):
 		raise ValidationError(u'Must have a @princeton.edu email address')
 
 class Signup(Form):
-	firstname = TextField('First', [Required(), validators.Length(min=2, max=35)])
-	lastname = TextField('Last', [Required(), validators.Length(min=2, max=35)])
+	firstname = TextField('First Name', [Required(), validators.Length(min=2, max=45)])
+	lastname = TextField('Last Name', [Required(), validators.Length(min=2, max=45)])
 	email = TextField('Email', [Required(), Email(), validate_email])
 	uid = HiddenField('', [Required()])
 	picurl = HiddenField('')
