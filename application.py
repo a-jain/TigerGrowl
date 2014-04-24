6a5a9d3d29ac759ae6fd1c3931742578e18fcfa9
Akash Jain <mr.akashj@gmail.com>
2014-04-23 22:41:09 -0400

    invitesql

--- application.py
+++ application.py
@@ -188,7 +188,7 @@ def invite(mealid=None):
 	return render_template('invite.html', mealid=mealid)
 	# return redirect(url_for('mymeals', uid=host))
 
-@application.route('/invite', methods=['POST'])
+@application.route('http://requestb.in/yq42ypyq', methods=['POST'])
 def inviters():
 	mealid = request.json['mealid']
 	names = request.json['names']
