a655d8cb4383fc6bedc7c0d1a3f41b3404de8293
kbruccoleri <kevinbruccoleri@gmail.com>
2014-04-28 23:47:39 -0400

    form update

--- form.py
+++ form.py
@@ -5,7 +5,7 @@ from wtforms_components import TimeField
 
 class MealForm(Form):
     host = TextField('Host', [Required(), validators.Length(min=3, max=45)])
-    place = TextField('Place', [Required(), validators.Length(min=3, max=45)])
+    place = SelectField(u'Choose a place', choices=[('forbes', 'Forbes'), ('mathey', 'Mathey'), ('rocky', 'Rocky'), ('whitman', 'Whitman'), ('wilcox', 'Wilcox'), ('wu', 'Wu'), ('cannon', 'Cannon'), ('capandgown', 'Cap and Gown'), ('charter', 'Charter'), ('cloister', 'Cloister'), ('colonial', 'Colonial'), ('cottage', 'Cottage'), ('ivy', 'Ivy'), ('quad', 'Quad'), ('terrace', 'Terrace'), ('tigerinn', 'Tiger Inn'), ('tower', 'Tower')])
     time = TimeField('Time', [Required()])
     date = DateField('Date (mm/dd)', format='%m/%d', validators=[Required()])
     priv = RadioField('Friends-only or Public', choices=[('pvt', 'Private'), ('pub', 'Public')])
