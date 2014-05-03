dee841c8cd6e6413439ee8405b842cb16a8dc096
gwalzer <gwalzer@princeton.edu>
2014-05-03 17:06:51 -0400

    a

--- application.py
+++ application.py
@@ -138,15 +138,15 @@ def joinmeal(uid=None, mealid=None):
 	hostid = meal[15]
 	
 	#type bashing
-		if (type(uid) is int):
-			str_uid = str(uid)
-		else:
-			str_uid = uid
-		
-		if (type(hostid) is int):
-			str_hostid = str(hostid)
-		else:
-			str_hostid = hostid
+	if (type(uid) is int):
+		str_uid = str(uid)
+	else:
+		str_uid = uid	
+	
+	if (type(hostid) is int):
+		str_hostid = str(hostid)
+	else:
+		str_hostid = hostid
 			
 	if str_hostid == str_uid: 
 			errorFlag = "3" # They are the host
@@ -232,19 +232,19 @@ def mymeals(uid=None, message=None):
 @application.route('/remove/<mealid>')
 @application.route('/remove/<mealid>/<uid>')
 def remove(mealid=None, uid=None):
-#	print("got to here1")
+	print("got to here1")
 	if not uid or not mealid:
 		return redirect(url_for('home'))
-#	print("got to here 2")
+	print("got to here 2")
 	# get the guest table for the given uid
 	cursor = db.cursor()
 	query = "SELECT * FROM ebdb.meal_table WHERE meal_id = %s;" % (mealid)
 	cursor.execute(query)
 	meal = cursor.fetchone()
-#	print("got to here 3")
+	print("got to here 3")
 	firstGuestIndex = 4 #hardcoded; this is the index of the first guest
 	guests = meal[firstGuestIndex:firstGuestIndex + 11]
-#	print("got to here 4")
+	print("got to here 4")
 	# search guests for uid
 	guest_X = 1
 	for guest in guests:
@@ -253,10 +253,10 @@ def remove(mealid=None, uid=None):
 			break
 		guest_X += 1
 
-#	print(guest_X)
-#	print(guest)
+	print(guest_X)
+	print(guest)
 	user_index = guest_X
-#	print("got to here 5")
+	print("got to here 5")
 	# search guests for final non-null array index
 	guest_Y = 0
 	for guest in guests:
@@ -265,7 +265,7 @@ def remove(mealid=None, uid=None):
 		if (not guest):
 			break
 		guest_Y += 1
-#	print("got to here 6")
+	print("got to here 6")
 	last_full_index = guest_Y
 	last_full = guests[last_full_index]
 	# The last_full_index will be -1 if the meal is empty. This should be impossible, so if we run into this problem then
@@ -273,19 +273,19 @@ def remove(mealid=None, uid=None):
 	
 	guestUIDString = "guest" + str(user_index)
 	guestLastString = "guest" + str(last_full_index)
-#	print("guestUIDString is")
-#	print(guestUIDString)
-#	print("guestLastString is")
-#	print(guestLastString)
-#	print("last_full is")
+	print("guestUIDString is")
+	print(guestUIDString)
+	print("guestLastString is")
+	print(guestLastString)
+	print("last_full is")
 #	# Now, update the uid at position user_index with uid at last_full_index.
 	sql = "UPDATE ebdb.meal_table SET %s = %s WHERE meal_id=%s;" % (guestUIDString, last_full, mealid)
 	cursor.execute(sql)
-#	print ("got to here 7")
+	print ("got to here 7")
 	# Then, update uid at position last_full_index with null.
 	sql = "UPDATE ebdb.meal_table SET %s = NULL WHERE meal_id=%s;" % (guestLastString, mealid)
 	cursor.execute(sql)
-#	print("got to here 8 - we've finished the removal (ostensibly)")
+	print("got to here 8 - we've finished the removal (ostensibly)")
 
 	return redirect(url_for('mymeals', uid=uid, message='success1'))
 	
