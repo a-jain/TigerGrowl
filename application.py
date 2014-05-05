f04a678c820bee01892c2209b2342a12c3c67a01
gwalzer <gwalzer@princeton.edu>
2014-05-04 21:43:07 -0400

    Signed-off-by: gwalzer <gwalzer@princeton.edu>

--- application.py
+++ application.py
@@ -355,11 +355,11 @@ def inviters(mealid=None):
 				is_invited_already = True
 		
 		#is the user already invited to this meal?
-		if not is_invited_already:
+		"""if not is_invited_already:
 			sql = "INSERT INTO ebdb.invitees (meal_id, guest) VALUES (%d, %d);" % (int(mealid), int(i))
 		 	cursor.execute(sql)
 		print i
-
+		"""
 	cursor.close()
 		
 	print "wtf"
