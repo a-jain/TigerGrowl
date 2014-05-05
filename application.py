9e43e855d780c1f391a0c20f65250696ef0328f5
Akash Jain <mr.akashj@gmail.com>
2014-05-05 17:31:07 -0400

    akash

--- application.py
+++ application.py
@@ -50,7 +50,7 @@ def timeline():
 @application.route('/feed/<errorFlag>')
 def feed(errorFlag=None):
 
-	clearOldMeals()
+	# clearOldMeals()
 	
 	cursor = db.cursor()
 	#What if page number gives an offset that is too large?
