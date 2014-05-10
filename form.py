ca8f33876cc1a5342430422455546e913b41f696
Akash Jain <mr.akashj@gmail.com>
2014-05-09 22:18:04 -0400

    datevalidator

--- form.py
+++ form.py
@@ -1,7 +1,7 @@
 from wtforms import Form, BooleanField, TextField, IntegerField, validators
 from wtforms.validators import ValidationError, Required, Email, URL
 from wtforms.fields import HiddenField, RadioField, SelectField
-from wtforms_components import TimeField
+from wtforms_components import TimeFieldk, DateTimeField
 
 from datetime import date
 from wtforms_html5 import DateField
@@ -10,7 +10,7 @@ class MealForm(Form):
     place = SelectField(u'Choose a place', choices=[('Forbes', 'Forbes'), ('Mathey', 'Mathey'), ('Rocky', 'Rocky'), ('Whitman', 'Whitman'), ('Wilcox', 'Wilcox'), ('Wu', 'Wu'), ('Cannon', 'Cannon'), ('Cap and Gown', 'Cap and Gown'), ('Charter', 'Charter'), ('Cloister', 'Cloister'), ('Colonial', 'Colonial'), ('Cottage', 'Cottage'), ('Ivy', 'Ivy'), ('Quad', 'Quad'), ('Terrace', 'Terrace'), ('Tiger Inn', 'Tiger Inn'), ('Tower', 'Tower')])
     time = TimeField('Time (hh:mm)', [Required()])
     # format='%m/%d/%y', 
-    date = DateField('Date (mm/dd)', validators=[Required()])
+    date = DateField('Date (mm/dd)', validators=[Required(), DateRange(min=datetime(2014, 01, 12), max=datetime(2015, 01, 02))])
     priv = RadioField('Friends-only or Public', [Required()], choices=[('pvt', 'Private'), ('pub', 'Public')])
     uid = HiddenField('')
 
