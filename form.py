95796699491fad234aac8d32daaa7df6e5ad6e2f
Akash Jain <mr.akashj@gmail.com>
2014-05-13 16:49:12 -0400

    akash

--- form.py
+++ form.py
@@ -13,13 +13,17 @@ class MealForm(Form):
 
 	place = SelectField(u'Choose a place', choices=[('Forbes', 'Forbes'), ('Mathey', 'Mathey'), ('Rocky', 'Rocky'), ('Whitman', 'Whitman'), ('Wilcox', 'Wilcox'), ('Wu', 'Wu'), ('Cannon', 'Cannon'), ('Cap and Gown', 'Cap and Gown'), ('Charter', 'Charter'), ('Cloister', 'Cloister'), ('Colonial', 'Colonial'), ('Cottage', 'Cottage'), ('Ivy', 'Ivy'), ('Quad', 'Quad'), ('Terrace', 'Terrace'), ('Tiger Inn', 'Tiger Inn'), ('Tower', 'Tower')])
 	time = TimeField('Time (hh:mm)', [Required(message=(u'Invalid Time'))])
-	date = DateField('Date (mm/dd/yyyy)', format='%m/%d/%Y', validators=[Required(message=(u'Invalid Date')), DateRange(min=datetime.now().date())])
+	date = DateField('Date (mm/dd/yyyy)', format='%m/%d/%Y', validators=[validate_date, Required(message=(u'Invalid Date')), DateRange(min=datetime.now().date())])
 	priv = RadioField('Friends-only or Public', [Required(message=(u'Public or Private?'))], choices=[('pvt', 'Private'), ('pub', 'Public')])
 	uid = HiddenField('', [Required()])
 
 def validate_email(form, field):
 	if "@princeton.edu" not in field.data.lower():
 		raise ValidationError(u'Must have a @princeton.edu email address')
+
+def validate_date(form, field):
+	if "2014" not in str(field.data) and "2015" not in str(field.data):
+		raise ValidationError(u'Don\'t get ahead of yourself!')
 
 class Signup(Form):
 	firstname = TextField('First Name', [Required(), validators.Length(min=1, max=44)])
