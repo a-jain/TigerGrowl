c964ef9669ce06284977e9105d3d50ed355df750
Akash Jain <mr.akashj@gmail.com>
2014-05-13 15:24:32 -0400

    kevin

--- form.py
+++ form.py
@@ -7,7 +7,7 @@ from datetime import date, datetime
 from wtforms_html5 import DateField, DateRange
 
 import locale
-locale.setlocale(locale.LC_ALL, 'en_GB.utf8')
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
 class MealForm(Form):
 
@@ -17,14 +17,8 @@ class MealForm(Form):
 
 	place = SelectField(u'Choose a place', choices=[('Forbes', 'Forbes'), ('Mathey', 'Mathey'), ('Rocky', 'Rocky'), ('Whitman', 'Whitman'), ('Wilcox', 'Wilcox'), ('Wu', 'Wu'), ('Cannon', 'Cannon'), ('Cap and Gown', 'Cap and Gown'), ('Charter', 'Charter'), ('Cloister', 'Cloister'), ('Colonial', 'Colonial'), ('Cottage', 'Cottage'), ('Ivy', 'Ivy'), ('Quad', 'Quad'), ('Terrace', 'Terrace'), ('Tiger Inn', 'Tiger Inn'), ('Tower', 'Tower')])
 	time = TimeField('Time (hh:mm)', [Required(message=(u'Invalid Time.'))])
-	# format='%m/%d/%y', 
-<<<<<<< HEAD
-	date = DateField('Date (mm/dd/yyyy) in the next month', format='%m/%d/%Y', validators=[DateRange(min=date(currentMonth, currentDay, currentYear), max=date((currentMonth+1) % 12, min(currentDay, 28)))], currentYear)
+	date = DateField('Date (mm/dd/yyyy) in the next month', format='%m/%d/%Y', validators=[DateRange(min=date(currentMonth, currentDay, currentYear), max=date((currentMonth+1) % 12, min(currentDay, 28), currentYear))])
 	priv = RadioField('Friends-only or Public', [Required()], choices=[('pvt', 'Private'), ('pub', 'Public')])
-=======
-	date = DateField('Date (mm/dd/yyyy) in the next month', format='%m/%d/%Y', validators=[Required(message=(u'Date is invalid.')), DateRange(min=date(currentYear, currentMonth, currentDay), max=date(currentYear, (currentMonth+1) % 12, min(currentDay, 28)))])
-	priv = RadioField('Friends-only or Public', [Required(message=(u'Private or public?'))], choices=[('pvt', 'Private'), ('pub', 'Public')])
->>>>>>> FETCH_HEAD
 	uid = HiddenField('')
 
 def validate_email(form, field):
